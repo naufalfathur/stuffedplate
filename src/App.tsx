5c037b99f1eb5bb6919dfb195a42e8617586836f
naufalfathur <naufalfathur@gmail.com>
2025-11-20 16:42:52 +0000

    react-vite-ts-tailwind-threefiber init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <div className='bg-black h-full w-full'>
+      <p className='text-white'>Hello Vite + React!</p>
+    </div>
+  )
+}
+
+export default App
