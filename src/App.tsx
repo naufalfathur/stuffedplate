99a3327fc8391e5d8f830a08d802ebf9bde4e87d
naufalfathur <naufalfathur@gmail.com>
2025-11-21 14:54:23 +0000

    fixed meal components, added gradient bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
-import { Canvas } from '@react-three/fiber'
-import { AccumulativeShadows, Environment, OrbitControls, RandomizedLight } from '@react-three/drei'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { AccumulativeShadows, Environment, OrbitControls, RandomizedLight, Text3D } from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
 import Plate from './components/plate'
 import Plane from './components/plane'
 import { Debug } from '@react-three/cannon'
 import Meal from './components/meal'
+import { useRef } from 'react'
+
 
 function App() {
+
   return (
-    <div className=' h-screen w-full'>
-      <div className='absolute bottom-5 left-5 z-10 text-gray-700 font-light'>
-        <h1 className='text-3xl'>Stuffed Plate</h1>
-        <p className='text-sm'>In my plate, I have ... </p>
+    <div className=' h-screen w-full relative bg-[linear-gradient(135deg,#FEAD8B,#EA523E)]'>
+      <div className="absolute top-0 z-10 w-full py-20 text-center text-gray-700">
+        <h1 className='text-4xl font-light'>Stuffed Plate</h1>
+        <p className='text-sm mt-2  font-light'>A fun physics-based 3D plate stuffing experience</p>
+      </div>
+      <div className='absolute bottom-20 left-5 z-10 text-gray-700 font-light w-full text-center'>
+        <p className='text-lg'>In my plate, I have ... </p>
       </div>
       <Canvas shadows camera={{ position: [-6, 8, 10], fov: 75 }}>
-        <color attach="background" args={['#f0f0f0']} />
+        {/* <color attach="background" args={['#f0f0f0']} /> */}
         <ambientLight intensity={0.5 * Math.PI} />
         <spotLight decay={0} position={[5, 5, -10]} angle={0.15} penumbra={1} />
         <pointLight decay={0} position={[-10, -10, -10]} />
+
 
         <Physics>
           <Debug color="red" scale={1.1}>
             <Plane position={[0, -0.49, 0]} />
 
-            <Meal name={'noodles'} mass={3} scale={0.25} args={[2.2, 0.3, 2.2]} offset={[0, 0, 0]} rotation={[5, 25, 10]} />
-            <Meal name={'bread'} mass={1} scale={0.25} args={[2.2, 0.2, 2.2]} offset={[0, -0.5, 0]} />
-            <Meal name={'salmon'} mass={5} scale={1} args={[2, 0.3, 1]} />
-            <Meal name={'salad'} mass={4} scale={25} args={[3, 0.2, 3]} />
-            <Meal name={'vegetable'} mass={2} scale={10} args={[1.5, 1, 1.5]} />
-            <Meal name={'beef'} mass={5} scale={0.1} args={[3, 0.4, 2]} />
-            <Meal name={'chicken'} mass={5} scale={1} args={[1, 0.8, 3]} />
-            <Meal name={'egg'} mass={3} scale={1} args={[2, 0.5, 3]} />
-            <Meal name={'rice'} mass={4} scale={1} />
-            <Meal name={'potato'} mass={3} scale={0.2} args={[3.5, 0.4, 3.5]} />
+            {/* <Meal name={'noodles'} />
+            <Meal name={'bread'} />
+            <Meal name={'salmon'} />
+            <Meal name={'salad'} />
+            <Meal name={'vegetable'} />
+            <Meal name={'beef'} />
+            <Meal name={'chicken'} />
+            <Meal name={'egg'} />
+            <Meal name={'rice'} />
+            <Meal name={'potato'} /> */}
 
             <Plate position={[0, 8, 0]} />
           </Debug>
--- src/components/meal.tsx
+++ src/components/meal.tsx
@@ -1,31 +1,38 @@
+import React, { useEffect } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { Mesh } from 'three'
 import { useBox, type BoxProps } from '@react-three/cannon'
+import { MEAL_CONFIG } from '../config/meals'
 
 interface MealProps extends BoxProps {
     name: string
-    scale: number
-    offset?: [number, number, number]
-    rotation?: [number, number, number]
 }
 
-function Meal(props: MealProps) {
-    const { scene } = useGLTF(`/models/meal/${props.name}.glb`)
-    const [ref] = useBox(() => ({ position: [0, 10, 0], ...props }))
+function Meal({ name, position, rotation, ...rest }: MealProps) {
+    const config = (MEAL_CONFIG as any)[name] ?? {}
+    const modelPath = `/models/meal/${name}.glb`
 
-    // enable shadows for all meshes
-    scene.traverse((child) => {
+    const { scene } = useGLTF(modelPath)
+
+    const startPosition = (position as any) ?? config.position ?? [0, 10, 0]
+    const args = config.args ?? [1, 1, 1]
+    const mass = config.mass ?? 1
+    const scale = config.scale ?? 1
+    const offset = config.offset ?? [0, 0, 0]
+    const visualRotation = (rotation as any) ?? config.rotation ?? [0, 0, 0]
+
+    const [ref] = useBox(() => ({ mass, args, position: startPosition, ...rest }))
+
+    scene.traverse((child: { castShadow: boolean; receiveShadow: boolean }) => {
         if (child instanceof Mesh) {
             child.castShadow = true
             child.receiveShadow = true
         }
     })
-    const OFFSET: [number, number, number] = props.offset || [0, 0, 0]
-    const ROTATION: [number, number, number] = props.rotation || [0, 0, 0]
 
     return (
         <group ref={ref}>
-            <group position={OFFSET} scale={props.scale} rotation={ROTATION}>
+            <group position={offset} rotation={visualRotation} scale={scale}>
                 <primitive object={scene} />
             </group>
         </group>
